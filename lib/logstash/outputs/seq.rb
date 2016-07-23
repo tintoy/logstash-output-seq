eabdda0298a4919e9366ed4e5c2554745e3724a5
tintoy <tintoy@tintoy.io>
2016-07-23 20:27:25 +1000

    Posting to Seq now works correctly.

--- lib/logstash/outputs/seq.rb
+++ lib/logstash/outputs/seq.rb
@@ -1,21 +1,22 @@
 # encoding: utf-8
-require "json/ext"
-require "logstash/outputs/base"
-require "logstash/plugin_mixins/http_client"
-require "logstash/namespace"
+require 'json/ext'
+require 'logstash/outputs/base'
+require 'logstash/plugin_mixins/http_client'
+require 'logstash/json'
+require 'logstash/namespace'
 
 # An output that sends events to Seq.
 class LogStash::Outputs::Seq < LogStash::Outputs::Base
   include LogStash::PluginMixins::HttpClient
 
   @@ignore_properties = {
-    "@timestamp" => true,
-    "@level" => true,
-    "@version" => true,
-    "message" => true
+      '@timestamp' => true,
+      '@level' => true,
+      '@version' => true,
+      'message' => true
   }
   
-  config_name "seq"
+  config_name 'seq'
 
   # The Seq server URL
   config :url, :validate => :string, :required => :true
@@ -27,63 +28,117 @@ class LogStash::Outputs::Seq < LogStash::Outputs::Base
   def register
     url = @url
 
-    url += "/" unless url.end_with? "/"
-    url += "api/events/raw"
+    url += '/' unless url.end_with? '/'
+    url += 'api/events/raw'
 
     @url = url
-  end # def register
 
-  # AF: Something odd about scoping here, need to work out how to pass instance method to_payload to lambda used by events.map.
+    @default_headers = {
+        'X-Seq-ApiKey' => @api_key,
+        'Content-Type' => 'application/json'
+    }
+
+    # We count outstanding requests with this queue; it tracks outstanding requests to create backpressure
+    # When this queue is empty no new requests may be sent, tokens must be added back by the client on success
+    @request_tokens = SizedQueue.new(@pool_max)
+    @pool_max.times {|t| @request_tokens << true }
+  end # def register
 
   public
   def multi_receive(events)
     payload = {
-      "Events" => events.map {|event| to_seq_payload(event)}
+        'Events' => events.map {|event| to_seq_payload(event)}
     }
-    payload = payload.to_json
+    post_to_seq(payload)
 
-    puts "Payload: #{payload}"
-
-  	# TODO: Submit to Seq.
-
-    return "#{events.length} events received"
+    "#{events.length} events received"
   end # def multi_receive
 
   public
   def receive(event)
-    payload = to_seq_payload(event)
-    payload = payload.to_json
+    payload = {
+        'Events' => [to_seq_payload(event)]
+    }
+    post_to_seq(payload)
 
-    puts "Payload: #{payload}"
-
-  	# TODO: Submit to Seq.
-
-    return "Event received"
+    'Event received'
   end # def receive
+
+  private
+  def post_to_seq(payload)
+    token = @request_tokens.pop
+
+    request = client.post(@url, {
+      headers: @default_headers,
+      body:    payload.to_json,
+      async:   true
+    })
+
+    request.on_complete do
+      @request_tokens << token
+    end
+
+    request.on_success do |response|
+      if response.code < 200 || response.code > 299
+        log_failure("Encountered non-200 HTTP code #{200}",
+          :response_code => response.code,
+          :url => url,
+          :event => event
+        )
+      end
+    end
+
+    request.on_failure do |exception|
+      log_failure("Could not fetch URL",
+        :url => url,
+        :method => @http_method,
+        :body => body,
+        :headers => headers,
+        :message => exception.message,
+        :class => exception.class.name,
+        :backtrace => exception.backtrace
+      )
+    end
+
+    request_async_background(request)
+  end # def post_to_seq
 
   # Convert a Logstash event to a Seq event payload.
   #
   # Note that we return a hash here, not the JSON, because it's more efficient to convert batched events to JSON all-in-one go.
   private
   def to_seq_payload(event)
-    props = {}
+    props = {
+        '@Version' => event['@version']
+    }
     payload = {
-      "@Version" => event["@version"],
-      "Timestamp" => event["@timestamp"],
-      "Level" => get_level(event),
-      "MessageTemplate" => event["message"],
-      "Properties" => props
+        :Timestamp => event['@timestamp'],
+        :Level => get_level(event),
+        :MessageTemplate => event['message'],
+        :Properties => props
     }
 
     event.instance_variable_get(:@data).each do |property, value|
       props[property] = value unless @@ignore_properties.has_key? property
     end
 
-    return payload
+    payload
   end # def to_seq_payload
 
   private
   def get_level(event)
-    return event["@level"] if event["@level"] else "Debug"
+    level = event['@level']
+
+    level ? level : 'Verbose'
+  end # def get_level
+
+  # Manticore doesn't provide a way to attach handlers to background or async requests well
+  # It wants you to use futures. The #async method kinda works but expects single thread batches
+  # and background only returns futures.
+  # Proposed fix to manticore here: https://github.com/cheald/manticore/issues/32
+  private
+  def request_async_background(request)
+    @method ||= client.executor.java_method(:submit, [java.util.concurrent.Callable.java_class])
+    @method.call(request)
   end
 end # class LogStash::Outputs::Seq
--- spec/outputs/seq_spec.rb
+++ spec/outputs/seq_spec.rb
@@ -1,51 +1,69 @@
 # encoding: utf-8
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/outputs/seq"
-require "logstash/codecs/plain"
-require "logstash/event"
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/outputs/seq'
+require 'logstash/codecs/plain'
+require 'logstash/event'
+
+require "sinatra"
+
+PORT = rand(65535-1024) + 1025
+
+# Enable access to request tokens from the test.
+class LogStash::Outputs::Seq
+  attr_reader :request_tokens
+end
 
 describe LogStash::Outputs::Seq do
-  let(:sample_event) do
-    return LogStash::Event.new({
-      "host" => "localhost",
-      "message" => "Hello Diddly"
+  let(:sample_event) {
+    LogStash::Event.new({
+      'host' => 'localhost',
+      'message' => 'Hi, Single Event'
     })
-  end
-  let(:sample_events) do
-    return [
+  }
+  let(:sample_events) {
+    [
       LogStash::Event.new({
-        "host" => "localhost",
-        "message" => "Hello World"
+        'host' => 'localhost',
+        'message' => 'Hello, World.',
+        'name' => 'World'
       }),
       LogStash::Event.new({
-        "host" => "localhost",
-        "message" => "Goodbye Moon"
-      }) 
+        'host' => 'localhost',
+        'message' => 'Goodbye Moon',
+        'name' => 'Moon'
+      })
     ]
-  end
-  let(:output) { LogStash::Outputs::Seq.new({"url" => "http://localhost:5432/"}) }
+  }
+  let(:output) {
+    LogStash::Outputs::Seq.new({
+      'url' => 'https://my-seq/',
+      'api_key' => 'my-api-key',
+      'ssl_certificate_validation' => false,
+      'pool_max' => 1
+    })
+  }
 
   before do
     output.register
   end
 
-  describe "receive message" do
+  describe 'receive message' do
     subject { output.receive(sample_event) }
 
-    it "returns a string" do
-      puts "Sample Event: #{sample_event.to_json}"
+    it 'returns a string' do
+      expect(subject).to eq('Event received')
 
-      expect(subject).to eq("Event received")
+      output.request_tokens.pop
     end
   end
 
-  describe "receive messages" do
+  describe 'receive messages' do
     subject { output.multi_receive(sample_events) }
 
-    it "returns a string" do
-      puts "Sample Events: #{sample_events.to_json}"
+    it 'returns a string' do
+      expect(subject).to eq('2 events received')
 
-      expect(subject).to eq("2 events received")
+      output.request_tokens.pop
     end
   end
 end
